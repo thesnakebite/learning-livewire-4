d31989a8c6b9650e01abcebd0664f4ae995f975b
Álvaro Navarro <alvaro@thesnakebite.es>
2026-03-28 19:52:18 +0100

    feat: add custom submit button with Livewire 4 loading states

--- resources/views/pages/post/⚡create.blade.php
+++ resources/views/pages/post/⚡create.blade.php
@@ -4,17 +4,20 @@ use App\Models\Post;
 use Livewire\Component;
 use Livewire\Attributes\Layout;
 
-new #[Layout('layouts::app', ['title' => 'Create post'])] class extends Component
-{
+new #[Layout('layouts::app', ['title' => 'Create post'])] class extends Component {
     public string $title = '';
     public string $content = '';
 
     public function save(): void
     {
-        Post::create($this->validate([
-            'title' => 'required|min:3',
-            'content' => 'required',
-        ]));
+        \Illuminate\Support\Sleep::sleep(1);
+
+        Post::create(
+            $this->validate([
+                'title' => 'required|min:3',
+                'content' => 'required',
+            ]),
+        );
 
         $this->redirect('/post/create');
     }
@@ -30,7 +33,12 @@ new #[Layout('layouts::app', ['title' => 'Create post'])] class extends Componen
         <flux:textarea wire:model="content" label="Content" />
 
         <div class="flex justify-end">
-            <flux:button type="submit" variant="primary" icon="paper-airplane" size="sm">Save post</flux:button>
+            <button type="submit"
+                class="data-loading:opacity-50 realtive inline-flex items-center justify-center gap-2 whitespace-nowrap font-medium text-sm h-10 rounded-lg ps-4 pe-4 bg-zinc-800 text-white border border-black/10 hover:bg-zinc-700 dark:bg-white dark:text-zinc-800 dark:hover:bg-zinc-200 cursor-pointer">
+                Create Post
+
+                <flux:icon.loading variant="micro" class="not-in-data-loading:hidden" />
+            </button>
         </div>
     </form>
 </div>
